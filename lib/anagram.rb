52270c5857e7c5756078e770ea0ffdff7d2ec5ab
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-30 23:37:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,17 @@
 # Your code goes here!
 class Anagram
+  
   def initialize(word)
-    @word = word
+    @wordcomp = word.split("").sort
   end
+  
+  def match(anagrams)
+    anagrams.each do |anagram|
+      if @wordcomp == anagram.split("").sort
+        return anagram
+      end
+    end
+    nil
+  end
+  
 end
